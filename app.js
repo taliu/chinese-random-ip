d00ed4528be2cfc31cd6779b22f349ad841fd886
taliu <taliu@outlook.com>
2014-11-03 21:37:41 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+﻿/*
+chineseIp.json的格式：
+{
+"provinceName1":[{min:"127.0.0.1",max:"128.0.0.1"},...],
+"provinceName2":[{min:"129.0.0.1",max:"130.0.0.1"},...],
+...
+}
+*/
+var chineseIps=require('./chineseIp.json');
+var ip = require('ip');
+/*
+getChineseIp([provinceArray|provinceName])
+
+概述
+	随机产生一个中国IP，可以通过一个可选省份列表或者省份名称来限定ip的产生范围
+参数
+	provinceArray为数组类型，是可选的，表示省份列表，用于指定ip产生范围
+	provinceName为字符串类型，是可选的，表示省份名称，用于指定ip产生范围
+返回值
+	返回一个随机的ip地址
+	如果指定的省份不存在，则返回null
+
+ *有效果的省份名称：
+上海,云南,内蒙,北京,台湾,吉林,四川,天津,宁夏,安徽,山东,山西,广东,广西,新疆,江苏
+江西,河北,河南,浙江,海南,湖北,湖南,甘肃,福建,西藏,贵州,辽宁,重庆,陕西,青海,香港
+黑龙江
+ * 
+*/
+function getChineseIp(provinceArray){
+	var provinceArray=provinceArray||getProvinces(chineseIps);
+	if(!(provinceArray instanceof Array)){//是一个省的名称时
+		provinceArray=[provinceArray];
+	}
+	var provinceName=getRandomItem(provinceArray);
+	var ipAddress=getProvinceRandomIp(provinceName);
+	return ipAddress;
+}
+ 
+
+//随机产生指定省的随机ip
+function getProvinceRandomIp(provinceName){
+	var ipAddress=null;
+	if(chineseIps.hasOwnProperty(provinceName)){
+		provinceIps=chineseIps[provinceName];
+	    var item=getRandomItem(provinceIps);//获取ip地址段
+		ipAddress=getRandomIp(item.min,item.max);
+	} 
+	return ipAddress;
+}
+
+//随机返回minIp到maxIp之间的ip，包括minIp和maxIp
+function getRandomIp(minIp, maxIp){
+	var min=ip.toLong(minIp);
+	var max=ip.toLong(maxIp);
+	var randomNum=getRandomNum(min,max+1);
+	return ip.fromLong(randomNum);
+}
+
+//获取中国各省市名称
+function getProvinces(chineseIps){
+	var provinces=[];
+	for(var province in chineseIps){
+		provinces.push(province);
+    }
+    return provinces;
+}
+
+//随机产生指定列表中的元素
+function getRandomItem(list){
+   var index=getRandomNum(0,list.length);
+   return list[index]; 
+}
+
+
+//随机返回min到max之间的整数，但不包括max
+function getRandomNum(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+exports.getChineseIp=getChineseIp;
+
+ 
---
+++ test/appSpec.js
@@ -0,0 +1,53 @@
+﻿
+var app = require("../../app.js");
+describe("中国ip随机生成器测试", function () {
+    it("随机生成一个中国IP", function () {
+        var chineseIp = app.getChineseIp();
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+
+        chineseIp = app.getChineseIp("");
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+
+        chineseIp = app.getChineseIp(null);
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+
+    });
+
+    it("生成结果为null", function () {
+        var chineseIp = app.getChineseIp({error:"error params"});
+        expect(chineseIp).toBe(null);
+
+        chineseIp = app.getChineseIp({});
+        expect(chineseIp).toBe(null);
+
+        chineseIp = app.getChineseIp("常山");
+        expect(chineseIp).toBe(null);
+
+        chineseIp = app.getChineseIp([]);
+        expect(chineseIp).toBe(null);
+    });
+
+    it("随机生成指定区域的ip", function () { 
+        var   chineseIp = app.getChineseIp("福建");
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+       
+        chineseIp = app.getChineseIp(["福建","浙江","江苏","广东"]);
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+
+        chineseIp = app.getChineseIp(["台湾","香港"]);
+        expect(chineseIp).toMatch(/(\d{1,3}\.){2}(\d{1,3}\.)/);
+    });
+
+    it("检查生成的IP地址是否准确", function (done) {//可能我们的ip地址库已经过时了，所有用淘宝ip来检查一下
+        var request = require('request');
+        var provinceName = "香港";
+        var chineseIp = app.getChineseIp(provinceName);
+        request('http://ip.taobao.com/service/getIpInfo.php?ip=' + chineseIp, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var ipInfo = JSON.parse(body);
+                expect(ipInfo.data.region).toContain(provinceName);
+                done();
+            }
+        });
+    });
+});
